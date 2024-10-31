6c4ef6d9380f5de736deddf9f20e794e2f353ccb
qianrui7426 <2575807392@qq.com>
2024-10-31 15:17:34 +0800

    修复Qt5阿西拜TCP服务器再下一次接受客户端连接时，未释放socket对象的bug

--- Qt Practice/AXiBaiProgram/14_02_TCPCommunication/Server/mainwindow.cpp
+++ Qt Practice/AXiBaiProgram/14_02_TCPCommunication/Server/mainwindow.cpp
@@ -149,7 +149,11 @@ void MainWindow::On_NewConnection()
         }
     });
 
-    m_pcTcpSocket->stateChanged(m_pcTcpSocket->state());            //刷新状态
+    connect(m_pcTcpSocket, &QTcpSocket::disconnected, this, [this](){
+        m_pcTcpSocket->deleteLater();                                   //断开时，需要在下一轮事件循环释放该 m_pcTcpSocket 对象
+    });
+
+    m_pcTcpSocket->stateChanged(m_pcTcpSocket->state());                //刷新状态
 }
 
 void MainWindow::closeEvent(QCloseEvent *event)
