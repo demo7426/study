fa6be7c81da26b76423fb47bbd13bdbad5d92e51
qianrui7426 <2575807392@qq.com>
2024-11-12 12:03:02 +0800

    新增masm6432视频课，在WDK 7600编译环境使用blk命令编译对应的win xp、win 2003、win 2008、win7 x86、win7 x64、win10 x86、win10 x64驱动

---
+++ WindowsDriverPratice/masm6432/02_01_安装驱动开发环境_使用命令行编译驱动/test.c
@@ -0,0 +1,39 @@
+/*************************************************
+Copyright (C), 2009-2012    , Level Chip Co., Ltd.
+文件名:	Delay.h
+作  者:	钱锐      版本: V0.1.0     新建日期: 2024.11.12
+描  述: 实现一个windows 驱动的demo
+备  注:  
+		1、编译驱动的方法
+		使用WDK 7600命令行终端,输入 bld 命令进行编译
+		
+		2、加载驱动的方法 
+		使用管理员权限打开cmd终端，使用 
+			sc create 驱动自定义名称 binpath= 路径 type= kernel
+			net start 驱动自定义名称								//加载驱动
+			net stop 驱动自定义名称								//卸载驱动
+			sc delete 驱动自定义名称								//删除驱动
+修改记录: 
+
+  1.  日期: 2024.11.12
+      作者: 钱锐
+      内容:
+          1) 此为模板第一个版本;
+      版本:V0.1.0
+
+*************************************************/
+
+#include <ntddk.h>
+
+void Unload(IN PDRIVER_OBJECT DriverObject)
+{
+	KdPrint(("Good bye Driver\n"));
+}
+
+NTSTATUS DriverEntry(IN PDRIVER_OBJECT DriverObject, IN PUNICODE_STRING RegistryPath)
+{
+	KdPrint(("Hello Driver\n"));
+	DriverObject->DriverUnload = Unload;
+	
+	return STATUS_SUCCESS;
+}
