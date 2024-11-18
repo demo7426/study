81c4742634b080ec029412883131a867da4b6cb5
qianrui7426 <2575807392@qq.com>
2024-11-18 21:24:06 +0800

    新增windows驱动masm6432,irp和派遣函数源码

---
+++ WindowsDriverPratice/masm6432/WinDiver/05_01_UserModeTest/main.cpp
@@ -0,0 +1,42 @@
+﻿/*************************************************
+Copyright (C), 2009-2012    , Level Chip Co., Ltd.
+文件名:	main.c
+作  者:	钱锐      版本: V0.1.0     新建日期: 2024.11.18
+描  述: irp和派遣函数
+备  注:
+修改记录:
+
+  1.  日期: 2024.11.18
+	  作者: 钱锐
+	  内容:
+		  1) 此为模板第一个版本;
+	  版本:V0.1.0
+
+*************************************************/
+#include <iostream>
+#include <Windows.h>
+#include <winioctl.h>
+
+using namespace std;
+
+#define IOTEST CTL_CODE(FILE_DEVICE_UNKNOWN, 0x800, METHOD_BUFFERED, FILE_ANY_ACCESS)
+
+int main()
+{
+	DWORD dwRet;
+	HANDLE hDevice = CreateFile(TEXT("\\\\.\\HelloDDK"), GENERIC_ALL, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+	if (hDevice == INVALID_HANDLE_VALUE)
+	{
+		cout << "设备打开失败, err:" << GetLastError() << endl;
+		return EXIT_FAILURE;
+	}
+
+	ReadFile(hDevice, NULL, 0, &dwRet, NULL);
+	WriteFile(hDevice, NULL, 0, &dwRet, NULL);
+	GetFileSize(hDevice, NULL);
+	DeviceIoControl(hDevice, IOTEST, NULL, 0, NULL, 0, &dwRet, NULL);
+	CloseHandle(hDevice);														//如果 hDevice 未正在被占用，则会先清理 hDevice，再关闭 hDevice
+
+	return EXIT_SUCCESS;
+}
+
---
+++ WindowsDriverPratice/masm6432/WinDiver/05_01_irp_DispatchFunc/main.c
@@ -0,0 +1,115 @@
+/*************************************************
+Copyright (C), 2009-2012    , Level Chip Co., Ltd.
+文件名:	main.c
+作  者:	钱锐      版本: V0.1.0     新建日期: 2024.11.18
+描  述: irp和派遣函数
+备  注: 
+修改记录:
+
+  1.  日期: 2024.11.18
+	  作者: 钱锐
+	  内容:
+		  1) 此为模板第一个版本;
+	  版本:V0.1.0
+
+*************************************************/
+
+#include <ntddk.h>
+
+/// <summary>
+/// 派遣函数
+/// </summary>
+/// <param name="DeviceObject"></param>
+/// <param name="Irp"></param>
+/// <returns></returns>
+NTSTATUS Driver_Dispatch(_In_ struct _DEVICE_OBJECT* DeviceObject, _Inout_ struct _IRP* Irp);
+
+VOID DriverUnload(IN PDRIVER_OBJECT DriverObject)
+{
+	UNICODE_STRING tSymbolicLinkName = RTL_CONSTANT_STRING(L"\\GLOBAL??\\HelloDDK");
+	
+	IoDeleteDevice(DriverObject->DeviceObject);
+	IoDeleteSymbolicLink(&tSymbolicLinkName);
+
+	KdPrint(("驱动卸载成功\n"));
+}
+
+NTSTATUS DriverEntry(IN PDRIVER_OBJECT DriverObject, IN PUNICODE_STRING RegistryPath)
+{
+	NTSTATUS lNTStatus = STATUS_SUCCESS;
+	UNICODE_STRING tDeviceName = RTL_CONSTANT_STRING(L"\\Device\\HelloDDK");
+	UNICODE_STRING tSymbolicLinkName = RTL_CONSTANT_STRING(L"\\GLOBAL??\\HelloDDK");
+	DEVICE_OBJECT tDeviceObject;
+	
+	lNTStatus = IoCreateDevice(DriverObject, 0, &tDeviceName, FILE_DEVICE_UNKNOWN, 0, FALSE, &tDeviceObject);
+	if (!NT_SUCCESS(lNTStatus))
+	{
+		KdPrint(("创建设备文件失败\n"));
+		return lNTStatus;
+	}
+
+	lNTStatus = IoCreateSymbolicLink(&tSymbolicLinkName, &tDeviceName);
+	if (!NT_SUCCESS(lNTStatus))
+	{
+		KdPrint(("创建符号链接文件失败\n"));
+		IoDeleteDevice(&tDeviceObject);
+		return lNTStatus;
+	}
+
+	for (size_t i = 0; i < 28; i++)
+	{
+		DriverObject->MajorFunction[i] = Driver_Dispatch;
+	}
+
+	DriverObject->DriverUnload = DriverUnload;
+
+	KdPrint(("驱动加载成功\n"));
+	return lNTStatus;
+}
+
+
+NTSTATUS Driver_Dispatch(_In_ struct _DEVICE_OBJECT* DeviceObject, _Inout_ struct _IRP* Irp)
+{
+	const char* pchIrpNames[] = {
+		"IRP_MJ_CREATE					   ",
+		"IRP_MJ_CREATE_NAMED_PIPE		   ",
+		"IRP_MJ_CLOSE					   ",
+		"IRP_MJ_READ					   ",
+		"IRP_MJ_WRITE					   ",
+		"IRP_MJ_QUERY_INFORMATION		   ",
+		"IRP_MJ_SET_INFORMATION			   ",
+		"IRP_MJ_QUERY_EA				   ",
+		"IRP_MJ_SET_EA					   ",
+		"IRP_MJ_FLUSH_BUFFERS			   ",
+		"IRP_MJ_QUERY_VOLUME_INFORMATION   ",
+		"IRP_MJ_SET_VOLUME_INFORMATION	   ",
+		"IRP_MJ_DIRECTORY_CONTROL		   ",
+		"IRP_MJ_FILE_SYSTEM_CONTROL		   ",
+		"IRP_MJ_DEVICE_CONTROL			   ",
+		"IRP_MJ_INTERNAL_DEVICE_CONTROL	   ",
+		"IRP_MJ_SHUTDOWN				   ",
+		"IRP_MJ_LOCK_CONTROL			   ",
+		"IRP_MJ_CLEANUP					   ",
+		"IRP_MJ_CREATE_MAILSLOT			   ",
+		"IRP_MJ_QUERY_SECURITY			   ",
+		"IRP_MJ_SET_SECURITY			   ",
+		"IRP_MJ_POWER					   ",
+		"IRP_MJ_SYSTEM_CONTROL			   ",
+		"IRP_MJ_DEVICE_CHANGE			   ",
+		"IRP_MJ_QUERY_QUOTA				   ",
+		"IRP_MJ_SET_QUOTA				   ",
+		"IRP_MJ_PNP						   ",
+		"IRP_MJ_PNP_POWER				   ",
+		"IRP_MJ_MAXIMUM_FUNCTION		   "
+	};
+
+	PIO_STACK_LOCATION pIOStack = IoGetCurrentIrpStackLocation(Irp);
+
+	KdPrint(("irp name:%s\n", pchIrpNames[pIOStack->MajorFunction]));
+
+	Irp->IoStatus.Information = 0;
+	Irp->IoStatus.Status = STATUS_SUCCESS;
+	IoCompleteRequest(Irp, IO_NO_INCREMENT);
+
+	return STATUS_SUCCESS;
+}
