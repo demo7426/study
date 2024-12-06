624fd251c2aecb5f039b34c17f3b330dd1fa4e55
demo7426 <2575807392@qq.com>
2024-12-07 00:30:44 +0800

    新增江科大STM32F103C8T6，控制OLED显示源码

---
+++ STM32F103C8T6_Practice/04_02_OLED显示器/User/main.c
@@ -0,0 +1,35 @@
+/*************************************************
+Copyright (C), 2009-2012    , Level Chip Co., Ltd.
+文件名:	main.c
+作  者:	钱锐      版本: V0.1.0     新建日期: 2024.12.07
+描  述: OLED显示数据
+备  注:  
+修改记录: 
+
+  1.  日期: 2024.12.07
+      作者: 钱锐
+      内容:
+          1) 此为模板第一个版本;
+      版本:V0.1.0
+
+*************************************************/
+
+#include "stm32f10x.h"                  // Device header
+
+#include "OLED.h"
+
+int main(void)
+{
+	OLED_Init();
+	
+	OLED_ShowChar(1, 1, 'A');
+	OLED_ShowString(1, 3, "HelloWorld");
+	OLED_ShowNum(2, 1, 12345, 5);
+	OLED_ShowSignedNum(2, 7, -66, 2);
+	OLED_ShowHexNum(3, 1, 0xAA55, 4);
+	OLED_ShowBinNum(4, 1, 0xAA55, 16);
+	
+	while(1)
+	{
+	}
+}
