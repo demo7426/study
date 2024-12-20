fe60f39e4be545df4409ff19881a1b33bf3f3a8b
qianrui7426 <2575807392@qq.com>
2024-12-20 20:43:17 +0800

    windows驱动masm6432,新增通过IoAllocateIrp生成irp源码，新增ObReferenceObjectByName获取设备对象（但win7测试失败，待解决）

--- WindowsDriverPratice/masm6432/WinDiver/10_DriverCallDriver/main.c
+++ WindowsDriverPratice/masm6432/WinDiver/10_DriverCallDriver/main.c
@@ -16,6 +16,20 @@ Copyright (C), 2009-2012    , Level Chip Co., Ltd.
 
 #include <ntddk.h>
 
+NTKERNELAPI
+NTSTATUS
+ObReferenceObjectByName(
+	IN PUNICODE_STRING ObjectName,
+	IN ULONG Attributes,
+	IN PACCESS_STATE PassedAccessState OPTIONAL,
+	IN ACCESS_MASK DesiredAccess OPTIONAL,
+	IN POBJECT_TYPE ObjectType,
+	IN KPROCESSOR_MODE AccessMode,
+	IN OUT PVOID ParseContext OPTIONAL,
+	OUT PVOID* Object
+);
+extern POBJECT_TYPE *IoDeviceObjectType;
+
 VOID APC_Routine(_In_ PVOID ApcContext, _In_ PIO_STATUS_BLOCK IoStatusBlock, _In_ ULONG Reserved)
 {
 	PKEVENT ptKEvent = (PKEVENT)ApcContext;
@@ -195,7 +209,7 @@ VOID CallDiver_Async_Test04(VOID)
 	pIrp->UserEvent = &tKEvent;
 
 	IoGetNextIrpStackLocation(pIrp)->FileObject = ptFileObj;
-
+	
 	lNTStatus = IoCallDriver(ptDevObj, pIrp);
 	if (lNTStatus == STATUS_PENDING)
 	{
@@ -204,6 +218,95 @@ VOID CallDiver_Async_Test04(VOID)
 	}
 
 	ObDereferenceObject(ptFileObj);
+}
+
+/// <summary>
+/// 通过设备指针，调用其它驱动函数测试
+/// </summary>
+/// <param name=""></param>
+VOID CallDiver_Async_Test05(VOID)
+{
+	UNICODE_STRING tDeviceName = RTL_CONSTANT_STRING(L"\\Device\\HelloDDK");				//设备对象名
+	UNICODE_STRING tSysbolicLinkName = RTL_CONSTANT_STRING(L"\\??\\HelloDDK");				//符号链接名
+	HANDLE handFile = NULL;
+	OBJECT_ATTRIBUTES tObjAttr;
+	IO_STATUS_BLOCK tIO_Status;
+	NTSTATUS lNTStatus = STATUS_SUCCESS;
+
+	LARGE_INTEGER tOffset = RtlConvertLongToLargeInteger(0);
+	KEVENT tKEvent;
+	PFILE_OBJECT ptFileObj = NULL;
+	PDEVICE_OBJECT ptDevObj = NULL;
+	PIRP pIrp = NULL;
+	PIO_STACK_LOCATION ptIO_Stack;
+
+	InitializeObjectAttributes(&tObjAttr, &tSysbolicLinkName, OBJ_KERNEL_HANDLE | OBJ_CASE_INSENSITIVE, NULL, NULL);
+
+	lNTStatus = IoGetDeviceObjectPointer(&tDeviceName, FILE_ANY_ACCESS, &ptFileObj, &ptDevObj);
+	if (!NT_SUCCESS(lNTStatus))
+	{
+		KdPrint(("获取设备对象指针出错\n"));
+		return;
+	}
+
+	KeInitializeEvent(&tKEvent, SynchronizationEvent, FALSE);
+
+	pIrp = IoAllocateIrp(ptDevObj->StackSize, FALSE);
+	if (pIrp)
+	{
+		pIrp->UserIosb = &tIO_Status;
+		pIrp->UserEvent = &tKEvent;
+		pIrp->AssociatedIrp.SystemBuffer = NULL;
+		pIrp->Tail.Overlay.Thread = PsGetCurrentThread();
+
+		ptIO_Stack = IoGetNextIrpStackLocation(pIrp);
+		ptIO_Stack->FileObject = ptFileObj;
+		ptIO_Stack->DeviceObject = ptDevObj;
+		ptIO_Stack->MajorFunction = IRP_MJ_READ;
+		ptIO_Stack->MinorFunction = IRP_MN_NORMAL;
+
+		lNTStatus = IoCallDriver(ptDevObj, pIrp);
+		if (lNTStatus == STATUS_PENDING)
+		{
+			KeWaitForSingleObject(&tKEvent, Executive, KernelMode, FALSE, NULL);
+			KdPrint(("完成读操作\n"));
+		}
+	}
+}
+
+/// <summary>
+/// 通过设备名称直接获取其指针，调用其它驱动函数测试
+/// </summary>
+/// <param name=""></param>
+VOID CallDiver_Async_Test06(VOID)
+{
+	UNICODE_STRING tDeviceName = RTL_CONSTANT_STRING(L"\\Device\\HelloDDK");				//设备对象名
+	UNICODE_STRING tSysbolicLinkName = RTL_CONSTANT_STRING(L"\\??\\HelloDDK");				//符号链接名
+	HANDLE handFile = NULL;
+	OBJECT_ATTRIBUTES tObjAttr;
+	IO_STATUS_BLOCK tIO_Status;
+	NTSTATUS lNTStatus = STATUS_SUCCESS;
+
+	LARGE_INTEGER tOffset = RtlConvertLongToLargeInteger(0);
+	KEVENT tKEvent;
+	PFILE_OBJECT ptFileObj = NULL;
+	PDEVICE_OBJECT ptDevObj;
+	PDRIVER_OBJECT ptDriverObj = NULL;
+	PIRP pIrp = NULL;
+	PIO_STACK_LOCATION ptIO_Stack;
+	
+	//TODO:win7专业版调用 ObReferenceObjectByName 返回 0xc0000024，待解决
+	lNTStatus = ObReferenceObjectByName(&tSysbolicLinkName, OBJ_CASE_INSENSITIVE, NULL, FILE_ANY_ACCESS, *IoDeviceObjectType, KernelMode, NULL, &ptDevObj);
+	if (NT_SUCCESS(lNTStatus))
+	{
+		KdPrint(("设备对象指针获取成功\n"));
+		KdPrint(("%p\n", ptDevObj));
+		ObDereferenceObject(ptDevObj);
+	}
+	else
+	{
+		KdPrint(("设备对象获取失败 0x%x\n", lNTStatus));
+	}
 }
 
 VOID DriverUnload(IN PDRIVER_OBJECT DriverObject)
@@ -223,7 +326,9 @@ NTSTATUS DriverEntry(IN PDRIVER_OBJECT DriverObject, IN PUNICODE_STRING Registry
 	//CallDiver_Sync_Test01();
 	//CallDiver_Async_Test02();
 	//CallDiver_Async_Test03();
-	CallDiver_Async_Test04();
+	//CallDiver_Async_Test04();
+	//CallDiver_Async_Test05();
+	CallDiver_Async_Test06();
 
 	return lNTStatus;
 }
