5e8593db804b0376173fd6ed1a511e6c8adc4c27
demo7426 <2575807392@qq.com>
2024-12-28 13:03:59 +0800

    江科大STM32F103C8T6，新增硬件I2C读写MPU6050源码注释

--- STM32F103C8T6_Practice/10_05_硬件I2C读写MPU6050/Hardware/MPU6050.c
+++ STM32F103C8T6_Practice/10_05_硬件I2C读写MPU6050/Hardware/MPU6050.c
@@ -44,28 +44,28 @@ void MPU6050_Init(void)
 	RCC_APB1PeriphClockCmd(RCC_APB1Periph_I2C2, ENABLE);
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
 	
-	tGPIO_InitTypeDef.GPIO_Mode = GPIO_Mode_AF_OD;				//设置为复用开漏输出
+	tGPIO_InitTypeDef.GPIO_Mode = GPIO_Mode_AF_OD;								//设置为复用开漏输出
 	tGPIO_InitTypeDef.GPIO_Pin = GPIO_Pin_10 | GPIO_Pin_11;
 	tGPIO_InitTypeDef.GPIO_Speed = GPIO_Speed_50MHz;
 	
-	GPIO_Init(GPIOB, &tGPIO_InitTypeDef);						//初始化GPIO
+	GPIO_Init(GPIOB, &tGPIO_InitTypeDef);										//初始化GPIO
 	
 	tI2C_InitTypeDef.I2C_Mode = I2C_Mode_I2C;
-	tI2C_InitTypeDef.I2C_ClockSpeed = 50000;
-	tI2C_InitTypeDef.I2C_DutyCycle = I2C_DutyCycle_2;
-	tI2C_InitTypeDef.I2C_Ack = I2C_Ack_Enable;
+	tI2C_InitTypeDef.I2C_ClockSpeed = 50000;									//当时钟频率小于100KHz时，占空比默认为1:1，此时I2C_DutyCycle用户设置无效
+	tI2C_InitTypeDef.I2C_DutyCycle = I2C_DutyCycle_2;							//设置占空比
+	tI2C_InitTypeDef.I2C_Ack = I2C_Ack_Enable;									//使能是否自动应答
 	tI2C_InitTypeDef.I2C_AcknowledgedAddress = I2C_AcknowledgedAddress_7bit;	//I2C自身地址长度
 	tI2C_InitTypeDef.I2C_OwnAddress1 = 0x00;									//I2C自身地址	
 	I2C_Init(I2C2, &tI2C_InitTypeDef);
 	
 	I2C_Cmd(I2C2, ENABLE);
 	
-	MPU6050_WriteReg(MPU6050_PWR_MGMT_1, 0x01);		//配置电源管理器1
-	MPU6050_WriteReg(MPU6050_PWR_MGMT_2, 0x00);		//配置电源管理器2
-	MPU6050_WriteReg(MPU6050_SMPLRT_DIV, 0x09);		//配置采样率分频；设置当前为10分频
-	MPU6050_WriteReg(MPU6050_CONFIG, 0x06);			//配置寄存器;取消外部同步,选择最平滑的滤波
-	MPU6050_WriteReg(MPU6050_GYRO_CONFIG, 0x18);	//不自测,陀螺仪传感器最大量程
-	MPU6050_WriteReg(MPU6050_ACCEL_CONFIG, 0x18);	//不自测,加速度传感器最大量程;取消高通滤波
+	MPU6050_WriteReg(MPU6050_PWR_MGMT_1, 0x01);									//配置电源管理器1
+	MPU6050_WriteReg(MPU6050_PWR_MGMT_2, 0x00);									//配置电源管理器2
+	MPU6050_WriteReg(MPU6050_SMPLRT_DIV, 0x09);									//配置采样率分频；设置当前为10分频
+	MPU6050_WriteReg(MPU6050_CONFIG, 0x06);										//配置寄存器;取消外部同步,选择最平滑的滤波
+	MPU6050_WriteReg(MPU6050_GYRO_CONFIG, 0x18);								//不自测,陀螺仪传感器最大量程
+	MPU6050_WriteReg(MPU6050_ACCEL_CONFIG, 0x18);								//不自测,加速度传感器最大量程;取消高通滤波
 }
 
 uint8_t MPU6050_ReadReg(uint8_t _Addr)
