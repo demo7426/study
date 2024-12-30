a6f081f223f78cdb92b19995fc89de9ad5bd29bd
qianrui7426 <2575807392@qq.com>
2024-12-30 12:14:27 +0800

    windows wdm驱动masm6432,新增wdm内核PNP处理源码

---
+++ WindowsDriverPratice/masm6432/WDMDriver/WDMDriver/02_06_PNPHandle/main.c
@@ -0,0 +1,201 @@
+/*************************************************
+Copyright (C), 2009-2012    , Level Chip Co., Ltd.
+文件名:	main.c
+作  者:	钱锐      版本: V0.0.1     新建日期: 2024.12.30
+描  述: PNP处理，改进WDM驱动
+备  注:
+修改记录:
+
+  1.  日期: 2024.12.30
+      作者: 钱锐
+      内容:
+          1) 此为模板第一个版本；
+      版本:V0.0.1
+
+*************************************************/
+
+#include <ntddk.h>
+#include <initguid.h>
+#include <stdlib.h>
+
+DEFINE_GUID(INTERFACEGUID,
+    0xA33317FD, 0xA262, 0x4E2E, 0xAB, 0x0F, 0x22, 0x8D, 0xC7, 0xDF, 0xF4, 0xAB);
+
+typedef struct _DEVICE_EXTENSION_INFO
+{
+    PDEVICE_OBJECT pFDO;                            //上层业务驱动
+    PDEVICE_OBJECT pPDO;                            //物理总线驱动
+    UNICODE_STRING SysbolicName;
+}DEVICE_EXTENSION_INFO, * PDEVICE_EXTENSION_INFO;
+
+NTSTATUS PNP_Default(_In_ struct _DEVICE_OBJECT* DeviceObject, _Inout_ struct _IRP* Irp)
+{
+    NTSTATUS lNTStatus = STATUS_SUCCESS;
+    PDEVICE_EXTENSION_INFO ptDevExtenInfo = DeviceObject->DeviceExtension;
+
+    IoSkipCurrentIrpStackLocation(Irp);
+    lNTStatus = IoCallDriver(ptDevExtenInfo->pPDO, Irp);
+
+    return lNTStatus;
+}
+
+NTSTATUS PNP_RemoveDevice(_In_ struct _DEVICE_OBJECT* DeviceObject, _Inout_ struct _IRP* Irp)
+{
+    NTSTATUS lNTStatus = STATUS_SUCCESS;
+    PDEVICE_EXTENSION_INFO ptDevExtenInfo = DeviceObject->DeviceExtension;
+
+    IoSkipCurrentIrpStackLocation(Irp);
+    lNTStatus = IoCallDriver(ptDevExtenInfo->pPDO, Irp);
+    IoDetachDevice(ptDevExtenInfo->pPDO);
+
+    IoDeleteDevice(ptDevExtenInfo->pFDO);
+    IoSetDeviceInterfaceState(&ptDevExtenInfo->SysbolicName, FALSE);
+
+    return lNTStatus;
+}
+
+NTSTATUS Driver_Dispatch(_In_ struct _DEVICE_OBJECT* DeviceObject, _Inout_ struct _IRP* Irp)
+{
+    NTSTATUS lNTStatus = STATUS_SUCCESS;
+
+    Irp->IoStatus.Information = 0;
+    Irp->IoStatus.Status = lNTStatus;
+    IoCompleteRequest(Irp, IO_NO_INCREMENT);
+
+    return lNTStatus;
+}
+
+NTSTATUS Driver_PNP_Dispatch(_In_ struct _DEVICE_OBJECT* DeviceObject, _Inout_ struct _IRP* Irp)
+{
+    NTSTATUS lNTStatus = STATUS_SUCCESS;
+    PDEVICE_EXTENSION_INFO ptDevExtenInfo = DeviceObject->DeviceExtension;
+    PIO_STACK_LOCATION ptIOStack = IoGetCurrentIrpStackLocation(Irp);
+
+    //PNP请求名称
+    static char* pchMinorFuncName[] = {
+        "IRP_MN_START_DEVICE                     ",
+        "IRP_MN_QUERY_REMOVE_DEVICE              ",
+        "IRP_MN_REMOVE_DEVICE                    ",
+        "IRP_MN_CANCEL_REMOVE_DEVICE             ",
+        "IRP_MN_STOP_DEVICE                      ",
+        "IRP_MN_QUERY_STOP_DEVICE                ",
+        "IRP_MN_CANCEL_STOP_DEVICE               ",
+        "IRP_MN_QUERY_DEVICE_RELATIONS           ",
+        "IRP_MN_QUERY_INTERFACE                  ",
+        "IRP_MN_QUERY_CAPABILITIES               ",
+        "IRP_MN_QUERY_RESOURCES                  ",
+        "IRP_MN_QUERY_RESOURCE_REQUIREMENTS      ",
+        "IRP_MN_QUERY_DEVICE_TEXT                ",
+        "IRP_MN_FILTER_RESOURCE_REQUIREMENTS     ",
+        "",
+        "IRP_MN_READ_CONFIG                      ",
+        "IRP_MN_WRITE_CONFIG                     ",
+        "IRP_MN_EJECT                            ",
+        "IRP_MN_SET_LOCK                         ",
+        "IRP_MN_QUERY_ID                         ",
+        "IRP_MN_QUERY_PNP_DEVICE_STATE           ",
+        "IRP_MN_QUERY_BUS_INFORMATION            ",
+        "IRP_MN_DEVICE_USAGE_NOTIFICATION        ",
+        "IRP_MN_SURPRISE_REMOVAL                 "
+    };
+
+    static NTSTATUS(*PNP_Handle[])(_In_ struct _DEVICE_OBJECT* DeviceObject, _Inout_ struct _IRP* Irp) = {
+        PNP_Default,
+        PNP_Default,
+        PNP_RemoveDevice,
+        PNP_Default,
+        PNP_Default,
+        PNP_Default,
+        PNP_Default,
+        PNP_Default,
+        PNP_Default,
+        PNP_Default,
+        PNP_Default,
+        PNP_Default,
+        PNP_Default,
+        PNP_Default,
+        PNP_Default,
+        PNP_Default,
+        PNP_Default,
+        PNP_Default,
+        PNP_Default,
+        PNP_Default,
+        PNP_Default,
+        PNP_Default,
+        PNP_Default,
+        PNP_Default
+    };
+
+    if (ptIOStack->MinorFunction >= _countof(pchMinorFuncName))
+        lNTStatus = PNP_Default(DeviceObject, Irp);                                 //必须接收总线驱动的返回值，否则会导致系统崩溃
+    else
+    {
+        KdPrint(("%s\n", pchMinorFuncName[ptIOStack->MinorFunction]));
+        lNTStatus = PNP_Handle[ptIOStack->MinorFunction](DeviceObject, Irp);        //必须接收总线驱动的返回值，否则会导致系统崩溃
+    }
+
+    return lNTStatus;
+}
+
+NTSTATUS Driver_AddDevice(_In_ struct _DRIVER_OBJECT* DriverObject, _In_ struct _DEVICE_OBJECT* PhysicalDeviceObject)
+{
+    NTSTATUS lNTStatus = STATUS_SUCCESS;
+    UNICODE_STRING tDevName = RTL_CONSTANT_STRING(L"\\Device\\HelloWDM");
+    UNICODE_STRING tSysbolicName = RTL_CONSTANT_STRING(L"\\??\\HelloWDM");
+    PDEVICE_OBJECT ptDevObj = NULL;
+    PDEVICE_EXTENSION_INFO ptDevExtenInfo = NULL;
+
+    lNTStatus = IoCreateDevice(DriverObject, sizeof(DEVICE_EXTENSION_INFO), NULL, FILE_DEVICE_UNKNOWN, FILE_AUTOGENERATED_DEVICE_NAME, FALSE, &ptDevObj);
+    if (!NT_SUCCESS(lNTStatus))
+    {
+        KdPrint(("创建设备文件失败\n"));
+        return lNTStatus;
+    }
+
+    ptDevExtenInfo = ptDevObj->DeviceExtension;
+    ptDevExtenInfo->pFDO = ptDevObj;
+    ptDevExtenInfo->pPDO = IoAttachDeviceToDeviceStack(ptDevObj, PhysicalDeviceObject);
+    if (ptDevExtenInfo->pPDO == NULL)
+    {
+        IoDeleteDevice(ptDevObj);
+        return STATUS_UNSUCCESSFUL;
+    }
+
+    //创建设备接口（符号链接）
+    IoRegisterDeviceInterface(ptDevExtenInfo->pPDO, &INTERFACEGUID, NULL, &ptDevExtenInfo->SysbolicName);
+    KdPrint(("%wZ\n", &ptDevExtenInfo->SysbolicName));
+    IoSetDeviceInterfaceState(&ptDevExtenInfo->SysbolicName, TRUE);
+
+    ptDevObj->Flags |= DO_BUFFERED_IO | DO_POWER_PAGABLE;
+    ptDevObj->Flags &= ~DO_DEVICE_INITIALIZING;
+
+    return lNTStatus;
+}
+
+VOID DriverUnload(IN PDRIVER_OBJECT DriverObject)
+{
+    KdPrint(("驱动卸载成功\n"));
+}
+
+NTSTATUS DriverEntry(IN PDRIVER_OBJECT DriverObject, IN PUNICODE_STRING RegistryPath)
+{
+    NTSTATUS lNTStatus = STATUS_SUCCESS;
+
+    DriverObject->DriverUnload = DriverUnload;
+
+    for (size_t i = 0; i <= IRP_MJ_MAXIMUM_FUNCTION; i++)
+    {
+        DriverObject->MajorFunction[i] = Driver_Dispatch;
+    }
+
+    DriverObject->MajorFunction[IRP_MJ_PNP] = Driver_PNP_Dispatch;
+    DriverObject->DriverExtension->AddDevice = Driver_AddDevice;
+
+    KdPrint(("驱动加载成功\n"));
+
+    return lNTStatus;
+}
+
+
+
+
