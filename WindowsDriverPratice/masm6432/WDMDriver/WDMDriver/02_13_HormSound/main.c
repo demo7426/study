2578fdd561b3feae918d959d8a8b6c04c41972f3
qianrui7426 <2575807392@qq.com>
2025-01-02 12:13:39 +0800

    windows wdm驱动masm6432,扩展wdm喇叭发声源码注释，及修改写入发声频率错误的bug

--- WindowsDriverPratice/masm6432/WDMDriver/WDMDriver/02_13_HormSound/main.c
+++ WindowsDriverPratice/masm6432/WDMDriver/WDMDriver/02_13_HormSound/main.c
@@ -2,9 +2,13 @@
 Copyright (C), 2009-2012    , Level Chip Co., Ltd.
 文件名:	main.c
 作  者:	钱锐      版本: V0.0.1     新建日期: 2025.01.01
-描  述: IO端口操作
-备  注: edx--端口地址
-        al、ax、eax--端口读取出的数据暂存位置
+描  述: 控制喇叭发声
+备  注: 端口地址      描述
+        0x40          计数器0
+        0x41          计数器1
+        0x42          计数器2
+        0x43          控制寄存器
+        0x61          喇叭控制
 修改记录:
 
   1.  日期: 2025.01.01
@@ -19,15 +23,15 @@ Copyright (C), 2009-2012    , Level Chip Co., Ltd.
 
 VOID Sound_On(INT32 _Freq)
 {
-    USHORT B = 1193180 / _Freq;
+    USHORT B = 1193180 / _Freq;                             //系统时钟晶振为1.1932MHz
     UCHAR uchTemp = READ_PORT_UCHAR((PUCHAR)0x61);
 
     uchTemp |= 3;
-    WRITE_PORT_UCHAR((PUCHAR)0x61, uchTemp);
-    WRITE_PORT_UCHAR((PUCHAR)0x43, 0xb6);
+    WRITE_PORT_UCHAR((PUCHAR)0x61, uchTemp);                //使能输出
+    WRITE_PORT_UCHAR((PUCHAR)0x43, 0xb6);                   //控制状态码
 
-    WRITE_PORT_UCHAR((PUCHAR)0x42, B & 0xff);
-    WRITE_PORT_UCHAR((PUCHAR)0x43, (B >> 8) & 0xff);
+    WRITE_PORT_UCHAR((PUCHAR)0x42, B & 0xff);               //系统分频;先写低八位，再写高八位（系统要求）
+    WRITE_PORT_UCHAR((PUCHAR)0x42, (B >> 8) & 0xff);
 }
 
 VOID Sound_Off(VOID)
