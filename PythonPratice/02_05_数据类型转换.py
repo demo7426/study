61454bb421ce4383b69f716833831b67d1a794b8
demo7426 <2575807392@qq.com>
2025-05-25 21:03:18 +0800

    Python基础学习,新增数据类型、数据类型转换、运算符的使用

---
+++ PythonPratice/02_05_数据类型转换.py
@@ -0,0 +1,22 @@
+# 将数据类型转换成字符串
+int_to_str = str(11)
+print(type(int_to_str), int_to_str)
+
+float_to_str = str(13.14)
+print(type(float_to_str), float_to_str)
+
+# 将字符串转换成数字
+str_to_int = int("123")
+print(type(str_to_int), str_to_int)
+
+str_to_float = float("13.14")
+print(type(str_to_float), str_to_float)
+
+# 将int转换为float
+int_to_float = float(11)
+print(type(int_to_float), int_to_float)
+
+# 将float转换为int
+float_to_int = int(13.14)
+print(type(float_to_int), float_to_int)
+
---
+++ PythonPratice/02_07_运算符.py
@@ -0,0 +1,28 @@
+# 算数(数学)运算符
+print("1 + 1 = ", 1 + 1)
+print("2 - 1 = ", 2 - 1)
+print("3 * 2 = ", 3 * 2)
+print("9 / 2 = ", 9 / 2)
+print("9 // 2 = ", 9 // 2)
+print("5 % 2 = ", 5 % 2)
+print("5 ** 2 = ", 5 ** 2)
+
+# 赋值运算符
+num = 1 + 2 * 3
+
+# 复合赋值运算符
+num += 1
+print("num += 1: ", num)
+num -= 1
+print("num -= 1: ", num)
+num *= 2
+print("num *= 2: ", num)
+num /= 2
+print("num /= 2: ", num)
+num //= 2
+print("num //= 2: ", num)
+num %= 2
+print("num %= 2: ", num)
+num = 2
+num **= 3
+print("num **= 2: ", num)
