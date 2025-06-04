683ace4d6528a9e932005c1cf46909f8893926c2
demo7426 <2575807392@qq.com>
2025-06-04 23:58:07 +0800

    正点原子_领航者_xc7Z020clg400-2,新增构建zynq最小系统，在串口输出hello_world源码工程

---
+++ Zynq/正点原子_领航者_xc7Z020clg400-2/vitis/hello_world/vitis/hello_world_system/_ide/scripts/debugger_hello_world-default.tcl
@@ -0,0 +1,27 @@
+# Usage with Vitis IDE:
+# In Vitis IDE create a Single Application Debug launch configuration,
+# change the debug type to 'Attach to running target' and provide this 
+# tcl script in 'Execute Script' option.
+# Path of this script: D:\Zynq\vitis\hello_world\vitis\hello_world_system\_ide\scripts\debugger_hello_world-default.tcl
+# 
+# 
+# Usage with xsct:
+# To debug using xsct, launch xsct and run below command
+# source D:\Zynq\vitis\hello_world\vitis\hello_world_system\_ide\scripts\debugger_hello_world-default.tcl
+# 
+connect -url tcp:127.0.0.1:3121
+targets -set -nocase -filter {name =~"APU*"}
+rst -system
+after 3000
+targets -set -nocase -filter {name =~"APU*"}
+loadhw -hw D:/Zynq/vitis/hello_world/vitis/system_wrapper/export/system_wrapper/hw/system_wrapper.xsa -mem-ranges [list {0x40000000 0xbfffffff}] -regs
+configparams force-mem-access 1
+targets -set -nocase -filter {name =~"APU*"}
+source D:/Zynq/vitis/hello_world/vitis/hello_world/_ide/psinit/ps7_init.tcl
+ps7_init
+ps7_post_config
+targets -set -nocase -filter {name =~ "*A9*#0"}
+dow D:/Zynq/vitis/hello_world/vitis/hello_world/Debug/hello_world.elf
+configparams force-mem-access 0
+targets -set -nocase -filter {name =~ "*A9*#0"}
+con
