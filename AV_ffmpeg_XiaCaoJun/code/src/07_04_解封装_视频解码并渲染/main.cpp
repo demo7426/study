2da91554d7d5e90a8518add66a1133750b22b2e5
qianrui7426 <2575807392@qq.com>
2025-08-26 11:49:02 +0800

    夏曹俊ffmpeg x265项目，实现一个对MP4格式其内部视频数据的解封装、渲染

---
+++ AV_ffmpeg_XiaCaoJun/code/src/07_04_解封装_视频解码并渲染/main.cpp
@@ -0,0 +1,208 @@
+﻿/*************************************************
+Copyright (C), 2009-2012    , Level Chip Co., Ltd.
+文件名:	main.cpp
+作  者:	钱锐      版本: V0.1.0     新建日期: 2025.08.26
+描  述: 实现一个对MP4格式其内部视频数据的解封装、渲染
+备  注: 
+修改记录:
+
+  1.  日期: 2025.08.26
+	  作者: 钱锐
+	  内容:
+		  1) 此为模板第一个版本;
+	  版本:V0.1.0
+
+*************************************************/
+
+#include <iostream>
+#include <thread>
+
+extern "C" {
+#include <libavformat/avformat.h>
+#include <libavcodec/codec.h>
+}
+
+#include "xdecode.h"
+#include "xvideo_view.h"
+
+#pragma comment(lib, "avformat.lib")
+#pragma comment(lib, "avcodec.lib")
+#pragma comment(lib, "avutil.lib")
+
+//打印错误信息
+static void PrintErr(int _ErrCode)
+{
+	if (_ErrCode == 0)				//正确时，不打印错误信息
+		return;
+
+	char chBuf[1024] = { 0 };
+	
+	av_strerror(_ErrCode, chBuf, sizeof(chBuf) - 1);
+
+	std::cerr << chBuf << std::endl;
+}
+
+int main()
+{
+	int nRtn = 0;
+	const char* pchURL = "1.mp4";						//媒体文件
+	AVFormatContext* ptAVFormatContext = nullptr;
+	AVPacket tAVPacket = { 0 };
+	AVStream* ptAVStream_Video = nullptr;				//视频流
+	AVStream* ptAVStream_Audio = nullptr;				//音频流
+
+	CXDeCode cXDeCode;
+
+	CXVideo_View* pcXVideo_View = nullptr;
+	CXVideo_View::Format eFmt = CXVideo_View::Format::YUV420P;
+
+	AVFrame* ptAVFrame = av_frame_alloc();
+
+	// 记录开始时间点
+	auto cStart_TimePoint = std::chrono::high_resolution_clock::now();
+	auto cEnd_TimePoint = cStart_TimePoint;
+
+	int nFrameCounter = 0;					//帧数计数
+
+	nRtn = avformat_open_input(
+		&ptAVFormatContext, 
+		pchURL,
+		nullptr,		//封装器格式;nullptr--自动探测，根据后缀名或者文件头判定其格式
+		nullptr			//参数设置，rtsp才需要设置
+	);
+	PrintErr(nRtn);
+
+	//获取媒体格式信息 无头部格式
+	nRtn = avformat_find_stream_info(ptAVFormatContext, nullptr);
+	PrintErr(nRtn);
+
+	//打印封装信息
+	av_dump_format(
+		ptAVFormatContext, 
+		0, 
+		pchURL, 
+		0		//上下文时输入还是输出;0--输入;1--输出
+	);
+
+	for (int i = 0; i < ptAVFormatContext->nb_streams; i++)
+	{
+		if (ptAVFormatContext->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_VIDEO)
+		{
+			ptAVStream_Video = ptAVFormatContext->streams[i];
+
+			std::cout << "------视频------" << std::endl;
+			std::cout <<"	width：" << ptAVStream_Video->codecpar->width << std::endl;
+			std::cout <<"	height：" << ptAVStream_Video->codecpar->height << std::endl;
+		}
+		else if (ptAVFormatContext->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_AUDIO)
+		{
+			ptAVStream_Audio = ptAVFormatContext->streams[i];
+
+			std::cout << "------音频------" << std::endl;
+			std::cout << "	sample_rate：" << ptAVStream_Audio->codecpar->sample_rate << std::endl;
+		}
+	}
+
+	if (ptAVStream_Video)
+	{
+		AVCodecID eVideoID = ptAVStream_Video->codecpar->codec_id;								//视频编码器ID
+		AVCodecParameters* ptAVCodecParameters = ptAVStream_Video->codecpar;					//视频编码参数
+
+		//视频解码初始化，因为后续解码是直接发送的AVPacket数据，在此处只能使用cpu解码
+		cXDeCode.Create_AVCodecContext(eVideoID, false);										
+		nRtn = avcodec_parameters_to_context(cXDeCode.GetAVCodecContext(), ptAVCodecParameters);		//解封装的视频编码参数，传递给解码上下文
+		if (nRtn < 0) 
+		{
+			PrintErr(nRtn);
+			return EXIT_FAILURE;
+		}
+
+		nRtn = cXDeCode.Open();
+		if (nRtn < 0)
+		{
+			PrintErr(nRtn);
+			return EXIT_FAILURE;
+		}
+	}
+
+	pcXVideo_View = CXVideo_View::Create();
+
+	bool bInitFlag = false;
+
+	while (1)
+	{
+		nRtn = av_read_frame(ptAVFormatContext, &tAVPacket);
+		if (nRtn < 0) 
+		{
+			PrintErr(nRtn);
+			break; // 读取结束或出错时退出
+		}
+		
+		if (ptAVStream_Video && tAVPacket.stream_index == ptAVStream_Video->index)
+		{
+			if (!cXDeCode.Send_AVPacket(&tAVPacket))
+				continue;
+
+			if (!cXDeCode.Recv_AVFrame(ptAVFrame))
+				continue;
+
+			//std::cout << ptAVFrame->pts << " ";
+
+			if (!bInitFlag)
+			{
+				switch (ptAVFrame->format)
+				{
+				case AVPixelFormat::AV_PIX_FMT_RGBA:
+					eFmt = CXVideo_View::Format::RGBA;
+					break;
+				case AVPixelFormat::AV_PIX_FMT_ARGB:
+					eFmt = CXVideo_View::Format::ARGB;
+					break;
+				case AVPixelFormat::AV_PIX_FMT_YUV420P:
+					eFmt = CXVideo_View::Format::YUV420P;
+					break;
+				case AVPixelFormat::AV_PIX_FMT_NV12:
+					eFmt = CXVideo_View::Format::NV12;
+					break;
+				default:
+					break;
+				}
+
+				pcXVideo_View->Init(ptAVFrame->width, ptAVFrame->height, eFmt, nullptr);
+				bInitFlag = true;
+			}
+
+			pcXVideo_View->DrawFrame(ptAVFrame);
+			nFrameCounter++;
+
+			av_frame_unref(ptAVFrame);
+		}
+
+		/*if (ptAVStream_Video && tAVPacket.stream_index == ptAVStream_Video->index)
+			std::cout << "视频：";
+		else if (ptAVStream_Audio && tAVPacket.stream_index == ptAVStream_Audio->index)
+			std::cout << "音频：";
+
+		std::cout << tAVPacket.pts << ", " << tAVPacket.dts << ", " << tAVPacket.size << std::endl;*/
+
+		av_packet_unref(&tAVPacket);
+		//std::this_thread::sleep_for(std::chrono::milliseconds(100));
+	
+
+		cEnd_TimePoint = std::chrono::high_resolution_clock::now();
+		if (std::chrono::duration_cast<std::chrono::milliseconds>(cEnd_TimePoint - cStart_TimePoint).count() >= 1000)
+		{
+			std::cout << "每秒帧数：" << nFrameCounter << std::endl;
+
+			nFrameCounter = 0;
+			cStart_TimePoint = cEnd_TimePoint;
+		}
+	}
+
+	avformat_close_input(&ptAVFormatContext);
+
+	av_frame_free(&ptAVFrame);
+
+	return EXIT_SUCCESS;
+}
+
---
+++ AV_ffmpeg_XiaCaoJun/code/src/07_04_解封装_视频解码并渲染/xencode.cpp
@@ -0,0 +1,263 @@
+/*************************************************
+Copyright (C), 2009-2012    , Level Chip Co., Ltd.
+文件名:	xencode.cpp
+作  者:	钱锐      版本: V0.1.0     新建日期: 2025.08.14
+描  述: 实现对ffmpeg编码过程的封装
+备  注:
+修改记录:
+
+  1.  日期: 2025.08.14
+	  作者: 钱锐
+	  内容:
+		  1) 此为模板第一个版本;
+	  版本:V0.1.0
+
+*************************************************/
+
+#include <iostream>
+
+#include "xencode.h"
+
+extern "C" {
+#include <libavcodec/avcodec.h>
+#include <libavutil/opt.h>
+}
+
+#pragma comment(lib, "avcodec.lib")
+#pragma comment(lib, "avutil.lib")
+
+CXEncode::CXEncode()
+{
+}
+
+CXEncode::~CXEncode()
+{
+	std::unique_lock<std::mutex> lock(m_mut);
+
+	av_frame_free(&m_ptAVFrame);
+	m_ptAVFrame = nullptr; 
+
+	avcodec_free_context(&m_ptAVCodecContext);
+	m_ptAVCodecContext = nullptr;
+}
+
+int CXEncode::Create_AVCodecContext(int _AVCodecID)
+{
+	AVCodecID eAVCodecID = static_cast<AVCodecID>(_AVCodecID);
+
+	std::unique_lock<std::mutex> lock(m_mut);
+
+	//找到编码器
+	AVCodec* ptAVCodec = avcodec_find_encoder(eAVCodecID);
+	if (!ptAVCodec)
+	{
+		std::cerr << "avcodec_find_encoder is err" << std::endl;
+		return -2;
+	}
+
+	//编码器上下文
+	m_ptAVCodecContext = avcodec_alloc_context3(ptAVCodec);
+	if (!m_ptAVCodecContext)
+	{
+		std::cerr << "avcodec_alloc_context3 is err" << std::endl;
+		return -2;
+	}
+
+	return 0;
+}
+
+AVFrame* CXEncode::Set_AVCodecContext_Param(int _Width, int _Height, int _FrameRate, int _AVPixelFormat, int _ThreadCount)
+{
+	std::unique_lock<std::mutex> lock(m_mut);
+
+	m_ptAVCodecContext->width = _Width;
+	m_ptAVCodecContext->height = _Height;
+	m_ptAVCodecContext->time_base = { 1, _FrameRate };								//帧时间戳的时间单位，比如1/30代表每秒30帧
+	m_ptAVCodecContext->pix_fmt = static_cast<AVPixelFormat>(_AVPixelFormat);		//源数据像素格式，与编程算法相关
+	m_ptAVCodecContext->thread_count = _ThreadCount;								//编码线程数
+
+	return Create_AVFrameAndAVPacket(m_ptAVCodecContext->width, m_ptAVCodecContext->height, m_ptAVCodecContext->pix_fmt);
+}
+
+int CXEncode::AV_Opt_Set(std::string _Name, std::string _Value)
+{
+	int nRtn = 0;
+
+	std::unique_lock<std::mutex> lock(m_mut);
+
+	nRtn = av_opt_set(m_ptAVCodecContext->priv_data, _Name.c_str(), _Value.c_str(), 0);
+	if (nRtn != 0)
+	{
+		char chErrBuf[1024] = { 0 };
+		av_strerror(nRtn, chErrBuf, sizeof(chErrBuf) - 1);
+
+		std::cerr << "av_opt_set is err" << std::endl;
+		std::cout << chErrBuf << std::endl;
+		return -2;
+	}
+
+	return 0;
+}
+
+int CXEncode::AV_Opt_Set_Int(std::string _Name, int _Value)
+{
+	int nRtn = 0;
+
+	std::unique_lock<std::mutex> lock(m_mut);
+
+	nRtn = av_opt_set_int(m_ptAVCodecContext->priv_data, _Name.c_str(), _Value, 0);
+	if (nRtn != 0)
+	{
+		char chErrBuf[1024] = { 0 };
+		av_strerror(nRtn, chErrBuf, sizeof(chErrBuf) - 1);
+
+		std::cerr << "av_opt_set_int is err" << std::endl;
+		std::cout << chErrBuf << std::endl;
+		return -2;
+	}
+
+	return 0;
+}
+
+int CXEncode::Open(void)
+{
+	int nRtn = 0;
+
+	std::unique_lock<std::mutex> lock(m_mut);
+
+	nRtn = avcodec_open2(m_ptAVCodecContext, NULL, NULL);
+	if (nRtn != 0)
+	{
+		char chErrBuf[1024] = { 0 };
+		av_strerror(nRtn, chErrBuf, sizeof(chErrBuf) - 1);
+
+		std::cerr << "avcodec_open2 is err" << std::endl;
+		std::cout << chErrBuf << std::endl;
+		return -2;
+	}
+
+	return 0;
+}
+
+AVPacket* CXEncode::SendFrame(AVFrame* _pAVFrame)
+{
+	int nRtn = 0;
+
+	std::unique_lock<std::mutex> lock(m_mut);
+
+	//发送未压缩数据到线程中压缩
+	nRtn = avcodec_send_frame(m_ptAVCodecContext, _pAVFrame);
+	if (nRtn)
+	{
+		return nullptr;
+	}
+
+	if (nRtn >= 0)		//接收一帧即可
+	{
+		AVPacket* ptAVPacket = av_packet_alloc();
+		if (!ptAVPacket)
+		{
+			std::cerr << "av_packet_alloc is err" << std::endl;
+			return nullptr;
+		}
+
+		//接受压缩帧，一遍前几次调用返回空（缓冲，立即返回，编码未完成）
+		//编码是运行在独立的线程中
+		//每次调用会重新分配ptAVPacket中的空间
+		nRtn = avcodec_receive_packet(m_ptAVCodecContext, ptAVPacket);
+		if (nRtn == AVERROR(EAGAIN) || nRtn == AVERROR_EOF)
+		{
+			return nullptr;
+		}
+
+		if (nRtn < 0)		//异常
+		{
+			char chErrBuf[1024] = { 0 };
+			av_strerror(nRtn, chErrBuf, sizeof(chErrBuf) - 1);
+
+			std::cerr << "av_frame_get_buffer is err" << std::endl;
+			std::cout << chErrBuf << std::endl;
+			return nullptr;
+		}
+
+		m_nFrameNum++;
+
+		return ptAVPacket;
+		//av_packet_unref(ptAVPacket);		//解引用后会自动释放AVPacket中分配的空间
+	}
+
+	return nullptr;
+}
+
+std::vector<AVPacket*> CXEncode::RecvAll_AVPacketData(void)
+{
+	int nRtn = 0;
+	
+	std::vector<AVPacket*> vecAVPacket = {};
+	std::unique_lock<std::mutex> lock(m_mut);
+
+	while (1)		//保证线程中编码后的剩余帧，全部取出并写入文件
+	{
+		AVPacket* ptAVPacket = av_packet_alloc();
+		if (!ptAVPacket)
+		{
+			std::cerr << "av_packet_alloc is err" << std::endl;
+			break;
+		}
+
+		//发送未压缩数据到线程中压缩
+		nRtn = avcodec_send_frame(m_ptAVCodecContext, NULL);		//不需要判定其返回值；因为需要保证线程中编码后的剩余帧，其数据全部被取出
+
+		//接受压缩帧，一遍前几次调用返回空（缓冲，立即返回，编码未完成）
+		//编码狮子啊独立的线程中
+		//每次调用会重新分配ptAVPacket中的空间
+		nRtn = avcodec_receive_packet(m_ptAVCodecContext, ptAVPacket);
+		if (nRtn == AVERROR(EAGAIN) || nRtn == AVERROR_EOF)
+		{
+			break;
+		}
+
+		if (nRtn < 0)		//异常
+		{
+			char chErrBuf[1024] = { 0 };
+			av_strerror(nRtn, chErrBuf, sizeof(chErrBuf) - 1);
+
+			std::cerr << "av_frame_get_buffer is err" << std::endl;
+			std::cout << chErrBuf << std::endl;
+			break;
+		}
+
+		m_nFrameNum++;
+
+		vecAVPacket.push_back(ptAVPacket);
+		//av_packet_unref(m_ptAVPacket);		//解引用后会自动释放AVPacket中分配的空间
+	}
+
+	std::cout << "帧数：" << m_nFrameNum << std::endl;
+	return vecAVPacket;
+}
+
+AVFrame* CXEncode::Create_AVFrameAndAVPacket(int _Width, int _Height, int _AVPixelFormat)
+{
+	int nRtn = 0;
+
+	//创建AvFrame空间 未压缩数据
+	m_ptAVFrame = av_frame_alloc();
+	m_ptAVFrame->width = _Width;
+	m_ptAVFrame->height = _Height;
+	m_ptAVFrame->format = _AVPixelFormat;
+
+	nRtn = av_frame_get_buffer(m_ptAVFrame, 0);
+	if (nRtn != 0)
+	{
+		char chErrBuf[1024] = { 0 };
+		av_strerror(nRtn, chErrBuf, sizeof(chErrBuf) - 1);
+
+		std::cerr << "av_frame_get_buffer is err" << std::endl;
+		std::cout << chErrBuf << std::endl;
+		return nullptr;
+	}
+
+
+	return m_ptAVFrame;
+}
