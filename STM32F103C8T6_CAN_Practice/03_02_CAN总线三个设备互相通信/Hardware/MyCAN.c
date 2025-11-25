1d65a508f5dc39738a227bbe4de245f3592da22e
demo7426 <2575807392@qq.com>
2025-11-26 00:16:28 +0800

    江科大STM32F103C8T6，新增CAN总线三个设备互相通信源码工程

---
+++ STM32F103C8T6_CAN_Practice/03_02_CAN总线三个设备互相通信/Hardware/MyCAN.c
@@ -0,0 +1,125 @@
+/*************************************************
+Copyright (C), 2009-2012    , Level Chip Co., Ltd.
+文件名:	MyCAN.c
+作  者:	钱锐      版本: V0.1.0     新建日期: 2025.11.25
+描  述: CAN协议数据收发模块
+备  注:  
+修改记录: 
+
+  1.  日期: 2025.11.25
+      作者: 钱锐
+      内容:
+          1) 此为模板第一个版本;
+      版本:V0.1.0
+
+*************************************************/
+
+#include <string.h>
+
+#include "MyCAN.h"
+
+void MyCAN_Init(void)
+{
+	GPIO_InitTypeDef tGPIO_InitTypeDef;
+	CAN_InitTypeDef tCAN_InitTypeDef;
+	CAN_FilterInitTypeDef tCAN_FilterInitTypeDef;
+	
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_CAN1, ENABLE);
+	
+	tGPIO_InitTypeDef.GPIO_Mode = GPIO_Mode_AF_PP;				//复用推挽输出
+	tGPIO_InitTypeDef.GPIO_Pin = GPIO_Pin_12;
+	tGPIO_InitTypeDef.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOA, &tGPIO_InitTypeDef);									//初始化GPIO
+	
+	tGPIO_InitTypeDef.GPIO_Mode = GPIO_Mode_IPU;					//复用上拉输出
+	tGPIO_InitTypeDef.GPIO_Pin = GPIO_Pin_11;
+	tGPIO_InitTypeDef.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOA, &tGPIO_InitTypeDef);									//初始化GPIO
+	
+	tCAN_InitTypeDef.CAN_Mode = CAN_Mode_Normal;
+	tCAN_InitTypeDef.CAN_Prescaler = 48;									//预分频;波特率 = 36MHz / 48 / (1 + 2 + 3) = 125KHz
+	tCAN_InitTypeDef.CAN_BS1 = CAN_BS1_2tq;	
+	tCAN_InitTypeDef.CAN_BS2 = CAN_BS2_3tq;
+	tCAN_InitTypeDef.CAN_SJW = CAN_SJW_2tq;
+	tCAN_InitTypeDef.CAN_NART = DISABLE;
+	tCAN_InitTypeDef.CAN_TXFP = DISABLE;
+	tCAN_InitTypeDef.CAN_RFLM = DISABLE;
+	tCAN_InitTypeDef.CAN_AWUM = DISABLE;
+	tCAN_InitTypeDef.CAN_TTCM = DISABLE;
+	tCAN_InitTypeDef.CAN_ABOM = DISABLE;
+	CAN_Init(CAN1, &tCAN_InitTypeDef);										//初始化CAN收发器
+	
+	tCAN_FilterInitTypeDef.CAN_FilterNumber = 0;
+	tCAN_FilterInitTypeDef.CAN_FilterIdHigh = 0x0000;
+	tCAN_FilterInitTypeDef.CAN_FilterIdLow = 0x0000;
+	tCAN_FilterInitTypeDef.CAN_FilterMaskIdHigh = 0x0000;
+	tCAN_FilterInitTypeDef.CAN_FilterMaskIdLow = 0x0000;
+	tCAN_FilterInitTypeDef.CAN_FilterScale = CAN_FilterScale_32bit;
+	tCAN_FilterInitTypeDef.CAN_FilterMode = CAN_FilterMode_IdMask;
+	tCAN_FilterInitTypeDef.CAN_FilterFIFOAssignment = CAN_Filter_FIFO0;
+	tCAN_FilterInitTypeDef.CAN_FilterActivation = ENABLE;
+	CAN_FilterInit(&tCAN_FilterInitTypeDef);							//初始化CAN过滤器
+}
+
+void MyCAN_Transmit(uint32_t _ID, uint8_t* _pData, uint8_t _Length)
+{
+	CanTxMsg tCanTxMsg;
+	
+	if(!_pData || _Length > 8)
+		return;
+	
+	tCanTxMsg.StdId = _ID;
+	tCanTxMsg.ExtId = _ID;
+	tCanTxMsg.IDE = CAN_Id_Standard;
+	tCanTxMsg.RTR = CAN_RTR_Data;			//帧类型
+	tCanTxMsg.DLC = _Length;					//数据载荷长度
+	
+	memcpy(tCanTxMsg.Data, _pData, _Length);
+	
+	CAN_Transmit(CAN1, &tCanTxMsg);
+}
+
+uint8_t MyCAN_ReceiveFlag(void)
+{
+	if(CAN_MessagePending(CAN1, CAN_FIFO0) > 0)
+		return 1;
+	
+	return 0;
+}
+
+void MyCAN_Receive(uint32_t* _pID, uint8_t _Data[8], uint8_t* _pLength)
+{
+	CanRxMsg tCanRxMsg;
+	
+	if(!_pID || !_Data || !_pLength)
+		return;
+	
+	CAN_Receive(CAN1, CAN_FIFO0, &tCanRxMsg);
+	
+	if(tCanRxMsg.IDE == CAN_Id_Standard)	//标准id
+	{
+		*_pID = tCanRxMsg.StdId;
+	}
+	else
+	{
+		*_pID = tCanRxMsg.ExtId;
+	}
+	
+	if(tCanRxMsg.RTR == CAN_RTR_Data)			//数据帧
+	{
+		*_pLength = tCanRxMsg.DLC;
+		memcpy(_Data, tCanRxMsg.Data, tCanRxMsg.DLC);
+	}
+	else																	//遥控帧
+	{
+		//...
+	}
+}
+
+
+
+
+
+
+
---
+++ STM32F103C8T6_CAN_Practice/03_02_CAN总线三个设备互相通信/User/main.c
@@ -0,0 +1,71 @@
+/*************************************************
+Copyright (C), 2009-2012    , Level Chip Co., Ltd.
+文件名:	main.c
+作  者:	钱锐      版本: V0.1.0     新建日期: 2025.11.25
+描  述: CAN总线回环测试
+备  注:  
+修改记录: 
+
+  1.  日期: 2025.11.25
+      作者: 钱锐
+      内容:
+          1) 此为模板第一个版本;
+      版本:V0.1.0
+
+*************************************************/
+
+#include "stm32f10x.h"                  // Device header
+
+#include "OLED.h"
+#include "Key.h"
+#include "MyCAN.h"
+
+int main(void)
+{
+	uint8_t uchKeyNum = 0;
+	
+	const uint32_t unTxID = 0x555;											//发送ID
+	uint8_t uchTxData[4] = {0x00, 0x01, 0x02,  0x03};		//发送数据缓冲区
+	
+	uint32_t unRxID = 0;																//接收ID
+	uint8_t uchRxLength = 0;														//接收数据的有效长度
+	uint8_t uchRxData[8] = { 0 }; 											//接收数据缓冲区
+	
+	OLED_Init();
+	Key_Init();
+	MyCAN_Init();
+	
+	OLED_ShowString(1, 1, "TxID:");
+	OLED_ShowString(2, 1, "RxID:");
+	OLED_ShowString(3, 1, "Leng:");
+	OLED_ShowString(4, 1, "Data:");
+	
+	OLED_ShowHexNum(1, 6, unTxID, 3);
+	
+	while(1)
+	{
+		uchKeyNum = Key_GetCurKey();
+			
+		if(uchKeyNum == 1)
+		{
+			MyCAN_Transmit(unTxID, uchTxData, 4);
+			
+			uchTxData[0]++;
+			uchTxData[1]++;
+			uchTxData[2]++;
+			uchTxData[3]++;
+		}
+		
+		if(MyCAN_ReceiveFlag() == 1)
+		{
+			MyCAN_Receive(&unRxID, uchRxData, &uchRxLength);
+			
+			OLED_ShowHexNum(2, 6, unRxID, 3);
+			OLED_ShowHexNum(3, 6, uchRxLength, 1);
+			OLED_ShowHexNum(4, 6, uchRxData[0], 2);
+			OLED_ShowHexNum(4, 9, uchRxData[1], 2);
+			OLED_ShowHexNum(4, 12, uchRxData[2], 2);
+			OLED_ShowHexNum(4, 15, uchRxData[3], 2);
+		}
+	}
+}
