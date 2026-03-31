59ae2152257d23800af436cdb4e2b8ae1256f575
qianrui7426 <2575807392@qq.com>
2026-03-31 15:26:34 +0800

    opencv4 c++ 快速入门视频30讲，新增图像像素值统计、图像几何形状绘制、随机数与随机颜色源码

---
+++ opencv/opencv4 c++ 快速入门视频30讲/14_图像几何形状绘制/main.cpp
@@ -0,0 +1,58 @@
+﻿/*************************************************
+Copyright (C), 2009-2012    , Level Chip Co., Ltd.
+文件名:	main.cpp
+作  者:	钱锐      版本: V0.1.0     新建日期: 2026.03.31
+描  述: 图像几何形状绘制
+备  注: 使用的是opencv4.12.0版本
+修改记录:
+
+  1.  日期: 2026.03.31
+	  作者: 钱锐
+	  内容:
+		  1) 此为模板第一个版本;
+	  版本:V0.1.0
+
+*************************************************/
+
+#include <iostream>
+
+#include "opencv2/opencv.hpp"
+
+int main()
+{
+	// 只显示错误及以上级别的日志，屏蔽 INFO
+	cv::utils::logging::setLogLevel(cv::utils::logging::LOG_LEVEL_WARNING);
+
+	cv::Mat cSrcMat = cv::imread("R-C.png");
+	if (cSrcMat.empty())
+	{
+		std::cout << __func__ << ", file is not exist" << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	cv::Mat cMat = cv::Mat::zeros(cSrcMat.size(), cSrcMat.type());
+	cv::Mat cDstMat;
+
+	cv::rectangle(cMat, cv::Rect(100, 100, 250, 300), cv::Scalar(0, 0, 255), -1, 8, 0);					//绘制矩形
+	cv::circle(cMat, cv::Point(350, 400), 15, cv::Scalar(255, 0, 0), -1, 8, 0);							//绘制圆形
+	cv::line(cMat, cv::Point(100, 100), cv::Point(350, 400), cv::Scalar(0, 255, 0), 4, cv::LINE_AA, 0);	//绘制线段
+
+	//绘制椭圆
+	cv::RotatedRect cRotatedRect;
+	cRotatedRect.center = cv::Point2f(200, 200);
+	cRotatedRect.size = cv::Size2f(100, 200);
+	cRotatedRect.angle = 90.0;
+
+	cv::ellipse(cMat, cRotatedRect, cv::Scalar(0, 255, 255), 2, cv::LINE_AA);
+
+	cv::addWeighted(cSrcMat, 0.7, cMat, 0.3, 0, cDstMat);
+
+	cv::imshow("合并后", cDstMat);
+	cv::waitKey(0);
+
+	cv::destroyAllWindows();
+
+	return EXIT_SUCCESS;
+}
+
+
---
+++ opencv/opencv4 c++ 快速入门视频30讲/15_随机数与随机颜色/main.cpp
@@ -0,0 +1,60 @@
+﻿/*************************************************
+Copyright (C), 2009-2012    , Level Chip Co., Ltd.
+文件名:	main.cpp
+作  者:	钱锐      版本: V0.1.0     新建日期: 2026.03.31
+描  述: 随机数与随机颜色
+备  注: 使用的是opencv4.12.0版本
+修改记录:
+
+  1.  日期: 2026.03.31
+	  作者: 钱锐
+	  内容:
+		  1) 此为模板第一个版本;
+	  版本:V0.1.0
+
+*************************************************/
+
+#include <iostream>
+
+#include "opencv2/opencv.hpp"
+
+int main()
+{
+	// 只显示错误及以上级别的日志，屏蔽 INFO
+	cv::utils::logging::setLogLevel(cv::utils::logging::LOG_LEVEL_WARNING);
+
+	int nWidth = 300;			//宽度
+	int nHeight = 400;			//高度
+
+	cv::RNG cRng;
+
+	cv::Mat cMat = cv::Mat::zeros(cv::Size(nWidth, nHeight), CV_8UC3);
+	int nInputKey = 0;			//输入的按键值
+
+	while (1)
+	{
+		nInputKey = cv::waitKey(100);
+		if (nInputKey == 27)				//esc键退出
+		{
+			break;
+		}
+
+		int x1 = cRng.uniform(0, nWidth);
+		int y1 = cRng.uniform(0, nHeight);
+		int x2 = cRng.uniform(0, nWidth);
+		int y2 = cRng.uniform(0, nHeight);
+		int r = cRng.uniform(0, 255);
+		int g = cRng.uniform(0, 255);
+		int b = cRng.uniform(0, 255);
+
+		cv::line(cMat, cv::Point(x1, y1), cv::Point(x2, y2), cv::Scalar(b, g, r), 1, cv::LINE_AA, 0);
+		cv::imshow("随机绘制演示", cMat);
+
+	}
+
+	cv::destroyAllWindows();
+
+	return EXIT_SUCCESS;
+}
+
+
